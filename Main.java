bc16e4590927388814ae728e58c100604bca3092
Hanish Kamakshigari <hanishkamakshigari@gmail.com>
2024-12-28 14:02:48 +0530

    past java practice

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 4;
+        int num3 = 9;
+        int sum = num1 + num2 + num3;
+        
+        System.out.println("Sum of the three digits are: " + sum);
+        String str = "Hanish";
+        System.out.println(str);
+    }
+}
