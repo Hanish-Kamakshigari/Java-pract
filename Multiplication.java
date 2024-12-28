ddd835aabac6c7cb5aa60839de06214022e76d10
Hanish Kamakshigari <hanishkamakshigari@gmail.com>
2024-12-28 14:10:13 +0530

    Added multiplication code

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int product=a*b;
+        System.out.println(product);
+    }
+}
