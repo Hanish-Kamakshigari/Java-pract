19a43318f31c17e673d63eef2aefdf0e2715dc10
Hanish Kamakshigari <hanishkamakshigari@gmail.com>
2024-12-28 16:23:23 +0530

    Arithmetic Operations

--- Multiplication.java
+++ Multiplication.java
@@ -3,6 +3,12 @@ public class Multiplication {
         int a=5;
         int b=6;
         int product=a*b;
+        int sum=a+b;
+        int difference= b-a;
+        int division = a/b;
         System.out.println(product);
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(division);
     }
 }
