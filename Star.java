d432c30025d7be6a75fec74d042e762f1506c977
Hanish Kamakshigari <hanishkamakshigari@gmail.com>
2025-01-07 22:38:45 +0530

    Star Pattern

---
+++ Star.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Star{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int i,j,r,c;
+        System.out.println("Enter the number of rows: ");
+        r=sc.nextInt();
+        System.out.println("Enter the choice");
+        System.out.println("1.Star Pattern");
+        System.out.println("2.Reverse star pattern");
+c=sc.nextInt();
+switch(c){
+    case 1:
+    for(i=1;i<=r;i++){
+        for(j=1;j<=i;j++){
+System.out.print("*");
+        }
+        System.out.println();
+    }
+    break;
+    case 2:
+    for(i=r;i>=1;i--){
+        for(j=1;j<=i;j++){
+System.out.print("*");
+        }
+        System.out.println();
+    }
+    break;
+    default:
+    System.out.println("Invalid choice");
+    break;
+}
+sc.close();
+    }
+}
